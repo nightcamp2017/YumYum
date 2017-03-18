2815c439510f9ee0c9dd19ddf13c17812b60274f
craigrabbitt <craigrabbitt@VIRTUALWIN10>
2017-03-18 15:00:27 +1300

    Add project files.

---
+++ YumYum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YumYum.Startup))]
+namespace YumYum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
