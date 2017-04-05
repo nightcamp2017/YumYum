e8852c1328342e207c9e153f74addad715b73bab
boebee <vivan_bee@hotmail.com>
2017-04-06 10:41:07 +1200

    Add odering system

--- YumYum/Controllers/HomeController.cs
+++ YumYum/Controllers/HomeController.cs
@@ -11,7 +11,9 @@ namespace YumYum.Controllers
     public class HomeController : Controller
     {
         YumYumEntities db = new YumYumEntities();
-
+        FoodList addedFoodObj = new FoodList();
+        List<FoodList> addedFoodList = new List<FoodList>() ;
+        
         public ActionResult Index()
         {
             return View();
@@ -83,16 +85,12 @@ namespace YumYum.Controllers
        
         public ActionResult FoodMenu()
         {
+            //desplay food category
             var foodCategory = db.FoodTypes.Select(x => new FoodList
             {
                 FoodCateId = x.FoodTypeId,
                 FoodCate = x.FoodType1
             }).ToList();
-
-
-            //FoodList model = new FoodList();
-            //model.FoodType = foodCategory;
-            //model.FoodItem = foodList;
              
              return View("FoodMenu",foodCategory);
 
@@ -100,11 +98,11 @@ namespace YumYum.Controllers
 
         public ActionResult GetFoodCate(int cateId)
         {
-           // Join FoodType and FoodItem tables using FoodTypeId
+            // Join FoodType and FoodItem tables using FoodTypeId
             var foodjoin =
                 from f in db.FoodTypes
                 join it in db.FoodItems on f.FoodTypeId equals it.FoodTypeId
-                select new 
+                select new
                 {
                     cate = it.FoodTypeId,
                     id = it.FoodItemId,
@@ -114,14 +112,65 @@ namespace YumYum.Controllers
                 };
             //Search food list in each category
             var eachCateTemp = foodjoin.Where(x => x.cate == cateId).ToList();
+           
             var eachCate = eachCateTemp.Select(x => new FoodList
             {
+                FoodCateId = x.cate,
                 FoodId = x.id,
                 FoodName = x.name,
                 FoodDetail =x.detail
             }).ToList();
-       
+
             return View(eachCate);
+        }
+        public ActionResult AddOrder(int itemId, int meatId)
+        {
+            //Take customers' selection of food and meat type
+            var selectedMeat = db.MeatTypes.Find(meatId);
+            var selectedFood = db.FoodItems.Find(itemId);
+
+            addedFoodObj.FoodCateId = selectedFood.FoodTypeId;
+            addedFoodObj.FoodId = selectedFood.FoodItemId;
+            addedFoodObj.FoodName = selectedFood.FoodName;
+            addedFoodObj.MeatId = selectedMeat.MeatId;
+            addedFoodObj.MeatOpt= selectedMeat.MeatType1.Trim();
+
+            //set Pricing depending on category and meat type
+            if (meatId == 1 || meatId == 2 || meatId == 3 || meatId == 5 && addedFoodObj.FoodCateId ==1 || addedFoodObj.FoodCateId ==3 || addedFoodObj.FoodCateId == 4)
+            {
+                addedFoodObj.PriceId = 1;
+            }
+            else
+            {
+                addedFoodObj.PriceId = 2;
+
+                if (meatId == 1 || meatId == 2 || meatId == 3 || meatId == 5 && addedFoodObj.FoodCateId == 2 || addedFoodObj.FoodCateId == 5)
+                {
+                    addedFoodObj.PriceId = 3;
+                }
+                else
+                {
+                    addedFoodObj.PriceId = 4;
+                }
+            }
+            //addedFoodList.Add(addedFoodObj);
+
+            return Json(addedFoodObj, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult SaveOrder(FoodList model)
+        {
+            //save order and update database
+            var saveOrder = new OrderDetail();
+            saveOrder.FoodItemId = model.FoodId;
+            saveOrder.MeatTypeId = model.MeatId;
+            saveOrder.SpicinessLevelId = model.HotId;
+            saveOrder.Quantity = model.Quantity;
+            saveOrder.PriceId = model.PriceId;
+            var addedOrderDetail = db.OrderDetails.Add(saveOrder);
+            db.SaveChanges();
+
+            return Json(new { success = true }, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- YumYum/Models/FoodList.cs
+++ YumYum/Models/FoodList.cs
@@ -13,10 +13,13 @@ namespace YumYum.Models
         public int FoodId { get; set; }
         public string FoodName { get; set; }
         public string FoodDetail { get; set; }
+        public int MeatId { get; set; }
         public string MeatOpt { get; set; }
         public int FoodCateId { get; set; }
         public string FoodCate { get; set; }
-        public decimal Price { get; set; }
-
+        public int PriceId { get; set; }
+        public int HotId { get; set; }
+        public string HotOpt { get; set; } 
+        public int Quantity { get; set; }
     }
 }
